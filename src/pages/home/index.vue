d15c43e3b666dada270ad9a03dbb45869e45f7f2
Airubby <ai_rubby@qq.com>
2020-02-04 11:11:58 +0800

    首页完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <view class="content">
-        <top info="home"></top>
+    <view>
         <view class="page-section-spacing">
             <swiper class="swiper" :indicator-dots="true" :autoplay="true" :interval="5000" :duration="500">
                 <swiper-item>
@@ -101,7 +100,198 @@
             </view>
         </view>
         <view>
-            
+            <view class="rank flex">
+                <text>排行榜</text>
+                <view class="rank-title">
+                    <view v-for="(tab,index) in tabBars" :key="'tab'+index" :class="['swiper-tab-list',tabIndex==index ? 'active' : '']"
+                    :id="'tab'+index" :data-current="index" @tap="tapTab">
+                    {{tab.name}}
+                    </view>
+                </view>
+            </view>
+            <swiper :current="tabIndex" style="height:160px;" :duration="300" @change="changeTab">
+                <swiper-item class="rank-con">
+                    <view class="rank-con-box">
+                        <view class="rank-img">
+                            <image src="/static/icons/rank-one.png" class="rank-icon" mode="widthFix"></image>
+                            <image src="/static/images/pimg.png" class="index-con-img" mode="scaleToFill"></image>
+                        </view>
+                        <view class="info">
+                            <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                            <view class="number"><text>24</text>人付款</view>
+                        </view>
+                    </view>
+                    <view class="rank-con-box">
+                        <view class="rank-img">
+                            <image src="/static/icons/rank-two.png" class="rank-icon" mode="widthFix"></image>
+                            <image src="/static/images/pimg.png" class="index-con-img" mode="scaleToFill"></image>
+                        </view>
+                        <view class="info">
+                            <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                            <view class="number"><text>24</text>人付款</view>
+                        </view>
+                    </view>
+                    <view class="rank-con-box">
+                        <view class="rank-img">
+                            <image src="/static/icons/rank-three.png" class="rank-icon" mode="widthFix"></image>
+                            <image src="/static/images/pimg.png" class="index-con-img" mode="scaleToFill"></image>
+                        </view>
+                        <view class="info">
+                            <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                            <view class="number"><text>24</text>人付款</view>
+                        </view>
+                    </view>
+                </swiper-item>
+                <swiper-item class="rank-con">
+                    <view class="rank-con-box">
+                        <view class="rank-img">
+                            <image src="/static/icons/rank-one.png" class="rank-icon" mode="widthFix"></image>
+                            <image src="/static/images/banner.png" class="index-con-img" mode="scaleToFill"></image>
+                        </view>
+                        <view class="info">
+                            <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                            <view class="number"><text>24</text>人付款</view>
+                        </view>
+                    </view>
+                    <view class="rank-con-box">
+                        <view class="rank-img">
+                            <image src="/static/icons/rank-two.png" class="rank-icon" mode="widthFix"></image>
+                            <image src="/static/images/banner.png" class="index-con-img" mode="scaleToFill"></image>
+                        </view>
+                        <view class="info">
+                            <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                            <view class="number"><text>24</text>人付款</view>
+                        </view>
+                    </view>
+                    <view class="rank-con-box">
+                        <view class="rank-img">
+                            <image src="/static/icons/rank-three.png" class="rank-icon" mode="widthFix"></image>
+                            <image src="/static/images/banner.png" class="index-con-img" mode="scaleToFill"></image>
+                        </view>
+                        <view class="info">
+                            <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                            <view class="number"><text>24</text>人付款</view>
+                        </view>
+                    </view>
+                </swiper-item>
+            </swiper>
+        </view>
+        <view>
+            <view class="index-title index-title1">猜您喜欢</view>
+            <view class="index-heart">
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+            </view>
+            <navigator class="toallProduct" url="/pages/allProduct/index" open-type="switchTab">
+                查看全部宝贝
+            </navigator>
+        </view>
+        <view>
+            <view class="index-title index-title1">逛逛更多宝贝</view>
+            <view class="index-heart">
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+                <view class="index-arscon">
+                    <image src="/static/images/pimg.png" class="index-con-img" mode="widthFix"></image>
+                    <view class="heart-con">
+                        <view class="title">长滩岛旅游项目魔术岛一日游水牛岛海钓海岛跳岛旅游浮潜深</view>
+                        <view class="nowprice">
+                            <view>￥<text class="number">152.00</text></view>
+                        </view>
+                        <view class="info flex">
+                            <view class="oldprice">182.99</view>
+                            <view>累计售出<text>24</text>件</view>
+                        </view>
+                    </view>
+                </view>
+            </view>
         </view>
     </view>
 </template>
@@ -117,11 +307,27 @@ export default {
     },
     data(){
         return{
-            
+            tabIndex:0,
+            tabBars: [{
+                name: '销量排行',
+            }, {
+                name: '收藏排行'
+            }],
+
         }
     },
 	methods: {
-        
+        async changeTab(e) {
+            let index = e.target.current;
+            this.tabIndex = index; //一旦访问data就会出问题
+        },
+        async tapTab(e) { //点击tab-bar
+            if (this.tabIndex === e.target.dataset.current) {
+                return false;
+            } else {
+                this.tabIndex = e.target.dataset.current
+            }
+        },
 	},
     watch: {
         
@@ -138,7 +344,12 @@ export default {
     .index-title{
         font-size: 16px;
         text-align: center;
-        margin: 15px 0 10px 0;
+        margin: 22px 0 10px 0;
+        color: #333;
+    }
+    .index-title1{
+        text-align: left;
+        padding-left: 10px;
     }
     .destination-con{
         display: flex;
@@ -198,5 +409,123 @@ export default {
                 margin: 10px 0;
             }
         }
+    }
+    .rank{
+        font-size: 16px;
+        padding: 0 10px;
+        margin: 22px 0 10px 0;
+        color: #333;
+        .rank-title{
+            display: flex;
+            .swiper-tab-list{
+                margin-left: 10px;
+                padding-bottom: 5px;
+                color: #999;
+                &.active{
+                    color: #333;
+                    border-bottom: 2px solid #FF5000;
+                }
+            }
+        }
+    }
+    .rank-con{
+        display: flex;
+        width: 100%;
+        padding: 0 4px;
+        .rank-img{
+            width: 100%;
+            height: 100px;
+            position: relative;
+            .rank-icon{
+                position: absolute;
+                left: 50%;
+                margin-left: -11px;
+                width: 22px;
+                bottom: -6px;
+                z-index: 999;
+            }
+        }
+        .rank-con-box{
+            width: 33.33%;
+            padding: 0 4px;
+            height: auto;
+            .info{
+                background: #fff;
+                overflow: hidden;
+                padding: 2px 2px 10px;
+                border-radius: 0 0 5px 5px;
+            }
+            .title{
+                height: 30px;
+                line-height: 32px;
+                overflow: hidden;
+                white-space: nowrap;
+                text-overflow: ellipsis;
+            }
+            .number{
+                color: #999;
+                font-size: 12px;
+            }
+        }
+    }
+    .index-con-img{
+        width: 100%;
+        height: 100px;
+        border-radius: 5px 5px 0 0;
+    }
+    .index-heart{
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0 4px 0;
+        .heart-con{
+            background: #fff;
+            padding: 10px 5px;
+            border-radius: 0 0 5px 5px;
+        }
+        .index-arscon{
+            width: calc(50% - 16px);
+            margin: 0 8px 10px;
+            .title{
+                position: relative;
+                flex-direction: column;
+                align-content: flex-start;
+                flex-shrink: 0;
+                font-size: 14px;
+                height:36px;
+                line-height: 18px;
+                overflow: hidden;
+                color: rgb(0, 0, 0);
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+                margin-bottom: 5px;
+            }
+            .nowprice{
+                font-size: 12px;
+                color: #FF5000;
+                .number{
+                    font-size: 16px;
+                }
+            }
+            .info{
+                font-size: 12px;
+                .oldprice{
+                    text-decoration: line-through;
+                    color: #999; 
+                }
+            }
+        }
+    }
+    .toallProduct{
+        background: #fff;
+        color: #333;
+        border-radius: 16px;
+        width: calc(100% - 20px);
+        height: 40px;
+        line-height: 40px;
+        margin: 0 auto;
+        text-align: center;
+        font-size: 16px;
     }
 </style>
---
+++ src/pages/index.vue
@@ -0,0 +1,213 @@
+<template>
+    <view class="content">
+        <view class="public-top">
+            <view class="public-top-con">
+                <view class="public-top-info">
+                    <view class="pbulic-logo-info">新疆国旅专营店<image src="/static/icons/public-top-right.png" style="width:14px;height:14px;margin-left:5px;" mode="widthFix"></image></view>
+                    <view class="public-top-attention">
+                        <image src="/static/icons/public-top-xin.png" style="width:18px;height:1px;margin-right:5px;" mode="widthFix"></image>关注
+                    </view>
+                </view>
+                <view class="font-size12">
+                    <image src="/static/icons/logo.png" style="width:16px;height:16px;margin-right:5px;" mode="widthFix"></image>粉丝数<text>234325</text>
+                </view>
+                <view class="public-top-nav">
+                    <navigator class="public-top-nav-search" url="/pages/register/index">
+                        <view class="search">
+                            <image src="/static/icons/search.png" style="width:18px;height:18px;margin-right:5px;" mode="widthFix"></image>搜索
+                        </view>
+                    </navigator>
+                    <view  class="public-scroll-navbar">
+                        <scroll-view scroll-x="true" :scroll-left="scrollLeft" class="public-scroll-navbar-con" @scroll="scroll" ref="scrollNar">
+                             <view v-for="(tab,index) in navBars" :key="'tab'+index" :class="['public-top-nav-list',navIndex==index ? 'active' : '']"
+                            :id="'tab'+index" :data-current="index" @tap="tapNav">
+                            {{tab.name}}
+                            </view>
+                            <!-- <navigator class="public-top-nav-list" :class="{'active':info=='home'}" url="/pages/home/index">
+                                首页
+                            </navigator>
+                            <navigator class="public-top-nav-list" :class="{'active':info=='video'}" url="/pages/video/index">
+                                玩法视频
+                            </navigator>
+                            <navigator class="public-top-nav-list" :class="{'active':info=='share'}" url="/pages/share/index">
+                                分享领钱
+                            </navigator>
+                            <navigator class="public-top-nav-list" :class="{'active':info=='event'}" url="/pages/event/index">
+                                活动
+                            </navigator>
+                            <navigator class="public-top-nav-list" :class="{'active':info=='mycenter'}" url="/pages/mycenter/index">
+                                个人中心
+                            </navigator> -->
+                        </scroll-view>
+                    </view>
+                </view>
+            </view>
+        </view>
+        <view class="fixed-box"></view>
+        <swiper :current="navIndex" class="swiper-box" :duration="300" @change="changeNav">
+            <swiper-item class="swiper-item">
+                <scroll-view scroll-y="true" :scroll-top="scrollTop" class="heightFull" @scroll="scrollNav">
+                    <home v-if="navIndex===0"></home>
+                </scroll-view>
+            </swiper-item>
+            <swiper-item class="swiper-item">
+                <scroll-view scroll-y="true" :scroll-top="scrollTop" class="heightFull" @scroll="scrollNav">
+                    <videoc v-if="navIndex===1"></videoc>
+                </scroll-view>
+            </swiper-item>
+            <swiper-item class="swiper-item">
+                <scroll-view scroll-y="true" :scroll-top="scrollTop" class="heightFull" @scroll="scrollNav">
+                    <share v-if="navIndex===2"></share>
+                </scroll-view>
+            </swiper-item>
+            <swiper-item class="swiper-item">
+                <scroll-view scroll-y="true" :scroll-top="scrollTop" class="heightFull" @scroll="scrollNav">
+                    <event v-if="navIndex===3"></event>
+                </scroll-view>
+            </swiper-item>
+            <swiper-item class="swiper-item">
+                <scroll-view scroll-y="true" :scroll-top="scrollTop" class="heightFull" @scroll="scrollNav">
+                    <mycenter v-if="navIndex===4"></mycenter>
+                </scroll-view>
+            </swiper-item>
+        </swiper>
+    </view>
+</template>
+
+<script>
+import home from '@/pages/home/index.vue'
+import videoc from '@/pages/video/index.vue'
+import share from '@/pages/share/index.vue'
+import event from '@/pages/event/index.vue'
+import mycenter from '@/pages/mycenter/index.vue'
+export default {
+    components: {home,videoc,share,event,mycenter},
+    created() {
+        
+    },
+    onShow(){
+        
+    },
+    onTabItemTap(){
+        this.scrollLeft=0;
+        this.scrollTop=0;
+        this.navIndex=0;
+    },
+    mounted() {
+    },
+    data(){
+        return{
+            navIndex:0,
+            scrollLeft:0,
+            scrollTop:0,
+            navBars: [{
+                name: '首页',
+            }, {
+                name: '玩法视频'
+            }, {
+                name: '分享领钱'
+            }, {
+                name: '活动'
+            }, {
+                name: '个人中心'
+            }],
+        }
+    },
+	methods: {
+        scroll:function(e){
+            this.scrollLeft = e.detail.scrollLeft
+        },
+        scrollNav:function(e){
+            this.scrollTop = e.detail.scrollTop
+        },
+        async changeNav(e) {
+            let index = e.target.current;
+            this.navIndex = index; //一旦访问data就会出问题
+        },
+        async tapNav(e) { //点击tab-bar
+            if (this.navIndex === e.target.dataset.current) {
+                return false;
+            } else {
+                this.navIndex = e.target.dataset.current
+            }
+        },
+	},
+    watch: {
+       
+    },
+    
+}
+</script>
+<style lang="less" scoped>
+    .fixed-box{
+        width: 100%;
+        height: 118px;
+    }
+    .public-top{
+        width: 100%;
+        height: 118px;
+        position: fixed;
+        z-index: 9999;
+        top: 0;
+        left: 0;
+        background: url("~@/static/images/bg.png") no-repeat;
+    }
+    .public-top-con{
+        width: 100%;
+        height: 100%;
+        padding: 12px 12px 0;
+        background: rgba(52, 51, 51, 0.86);
+        color: #fff;
+    }
+    .public-top-info{
+        font-size: 18px;
+        display: flex;
+        justify-content: space-between;
+    }
+    .public-top-attention{
+        font-size: 14px;
+        border: 1px solid #818489;
+        padding: 3px 6px;
+        border-radius: 30px;
+        background: #414449;
+    }
+    .public-top-nav{
+        font-size: 16px;
+        margin-top: 16px;
+        display: flex;
+    }
+    .public-top-nav-search{
+        display: inline-block;
+        width: 85px;
+    }
+    .public-top-nav-search .search{
+        display: inline-block;
+        background: #939496;
+        padding: 3px 6px;
+        border-radius: 30px;
+    }
+    .public-scroll-navbar{
+        width: calc(100% - 85px);
+        height: 30px;
+        line-height: 28px;
+    }
+    .public-scroll-navbar-con{
+        width: 100%;
+        height: 100%;
+        white-space:nowrap;
+    }
+    .public-top-nav-list{
+        display: inline-block;
+        margin-right: 16px;
+        &.active{
+            border-bottom: 2px solid #fff;
+        }
+    }
+    .swiper-box{
+        width: 100%;
+        height: calc(100% - 118px);
+    }
+    .heightFull{
+        height: 100%;
+    }
+</style>
